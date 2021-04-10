bee8474a198d0ef180c30dcc23145886579b5a4e
Rob O'Gara <rgo594@gmail.com>
2021-04-10 14:02:00 -0400

    made it so you could have different sprites for the mapped tile prefab when adding tile assets to the palette

--- Assets/Scripts/TilePlacer.cs
+++ Assets/Scripts/TilePlacer.cs
@@ -4,15 +4,19 @@ using UnityEngine.Tilemaps;
 using System.Collections;
 using System.Collections.Generic;
 
-[CreateAssetMenu(menuName = "My Assets/COCKASDFA")]
+[CreateAssetMenu(menuName = "My Assets/Create Action Tile")]
 public class TilePlacer : Tile
 {
-
+    //public Sprite spriteA;
 
     public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
     {
-        tileData.sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+
         tileData.gameObject = gameObject;
+        tileData.sprite = sprite;
+        //tileData.gameObject.GetComponent<SpriteRenderer>().sprite = tileData.sprite;
+        //gameObject.GetComponent<SpriteRenderer>().sprite = spriteA;
+        //tileData.sprite = spriteA;
     }
 
 }
